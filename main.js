229e1e6e6b9405e893bf70e7606aeb12e0e482b7
Seek <solverseek666@gmail.com>
2026-08-19 14:39:00 +0800

    Create main.js
    huh lol

---
+++ main.js
@@ -0,0 +1,9 @@
+var gameData = {
+  starterNum: 0,
+  number: 0
+}
+function incretiny() {
+  gameData.starterNum += 1
+}
+var starterList = ["0","ε","BITG","Oe(54)","1/{10,10[2]2}","1/{10,10,10,10}","(g<sub>64</sub>)<sup>-1</sup>","1/{10,10,100}","1/(10^^100)","10<sup>-100</sup>","0.0000000001"]
+document.getElementById("number").innerHTML = starterList[gameData.starterNum]
