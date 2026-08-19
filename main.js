05d2d37abca40da431470b83b1c5627f3e765f06
Seek <solverseek666@gmail.com>
2026-08-19 23:43:02 +0800

    Update main.js
    lhguktugkjlol

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var gameData = {
   starterNum: 0,
-  NormalNum: 0
+  normalNum: 0
 }
 var starterList = ["0","ε","BITG","Oe(54)","1/{10,10[2]2}","1/{10,10,10,10}","(g<sub>64</sub>)<sup>-1</sup>","1/{10,10,100}","1/(10^^100)","10<sup>-100</sup>"]
 function incretiny() {
@@ -8,7 +8,7 @@ function incretiny() {
     gameData.starterNum += 1
     document.getElementById("number").innerHTML = starterList[gameData.starterNum]
   } else {
-    gameData.starterNum = 0.0000000001
-    document.getElementById("number").innerHTML = gameData.starterNum.toFixed()
+    gameData.normalNum = 0.0000000001
+    document.getElementById("number").innerHTML = gameData.normalNum.toFixed()
   }
 }
