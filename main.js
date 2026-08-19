93d2ec22d5ac9ae4bc813ef9fb7e000e295e72d6
Seek <solverseek666@gmail.com>
2026-08-19 23:05:44 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var gameData = {
 }
 var starterList = ["0","ε","BITG","Oe(54)","1/{10,10[2]2}","1/{10,10,10,10}","(g<sub>64</sub>)<sup>-1</sup>","1/{10,10,100}","1/(10^^100)","10<sup>-100</sup>"]
 function incretiny() {
-  if gameData.starterNum < 10 {
+  if (gameData.starterNum < 10) {
     gameData.starterNum += 1
     document.getElementById("number").innerHTML = starterList[gameData.starterNum]
   } else {
