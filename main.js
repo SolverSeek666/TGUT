7ccf53b3ece317c0c1b33edf16cf215f68240e6f
Seek <solverseek666@gmail.com>
2026-08-20 17:17:37 +0800

    Update main.js
    hehe

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ var game = {
   exp: 1
 }
 // =================================================
-var starterList = ["0","ε","BITG","Oe(54)","1/{10,10[2]2}","1/{10,10,10,10}","(g<sub>64</sub>)<sup>-1</sup>","1/{10,10,100}","1/(10^^100)","10<sup>-100</sup>","0.0000000001"]
+var starterList = ["0","ε","BITG","Oe(54)","1/{10,10[2]2}","1/{10,10,10,10}","(g<sub>64</sub>)<sup>-1</sup>","1/{10,10,100}","1/(10^^100)","10<sup>-100</sup>"]
 // =================================================
 function formatNum(decimal) {
     if (decimal < 0.001) {
@@ -25,7 +25,8 @@ function formatNum(decimal) {
 }
 // =================================================
 function UP1() {
-  if (game.upNeg10Count >= game.upNeg10Max) {
+  if (game.upNeg10Count >= game.upNeg10Max-1) {
+    game.upNeg10Count += 1
     game.numbers = 0.0000000001
     document.getElementById("number").innerHTML = formatNum(game.numbers);
     document.getElementById("up1-cost").innerHTML = "Upgrade Maxed";
