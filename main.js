b3c8bb67d1efb081816ed826e5c7793891f39dca
Seek <solverseek666@gmail.com>
2026-08-21 12:21:36 +0800

    Update main.js
    hmmmmmmmmmmmmmmmmmmmmmmmm

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 var game = {
   numbers: 0,
   
+  growth: 0,
+  add: 0,
+  mult: 1,
+  exp: 1,
+  
+  seconds: 0,
+  minutes: 0,
+  hours: 0,
+  days: 0,
+  years: 0,
+  
   upNeg10Count: 0,
   upNeg9Count: 0,
   upNeg8Count: 0,
@@ -13,11 +24,14 @@ var game = {
   upNeg1Count: 0,
   
   up0Count: 0,
-  
-  growth: 0,
-  add: 0,
-  mult: 1,
-  exp: 1
+}
+// =================================================
+function time() {
+  const startTime = Date.now();  
+  setInterval(() => {
+    const currentTime = Date.now();
+    game.seconds = (currentTime - startTime) / 1000;
+  }, 1);
 }
 // =================================================
 var starterList = ["0","ε","BITG","Oe(54)","1/{10,10[2]2}","1/{10,10,10,10}","(g<sub>64</sub>)<sup>-1</sup>","1/{10,10,100}","1/(10^^100)","10<sup>-100</sup>"]
