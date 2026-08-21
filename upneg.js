63fa50b1e9d45430617ead070bb21441025664fb
Seek <solverseek666@gmail.com>
2026-08-21 09:57:16 +0800

    Update upneg.js
    hyper

--- upneg.js
+++ upneg.js
@@ -8,7 +8,7 @@ for (let i = 0; i < negpanel.length; i++) {
 }
 // =================================================
 function UPneg10() {
-  if (game.upNeg10Count >= game.upNeg10Max-1) {
+  if (game.upNeg10Count >= 10-1) {
     game.upNeg10Count += 1
     game.numbers = 0.0000000001
     document.getElementById("number").innerHTML = formatNum(game.numbers);
@@ -24,26 +24,7 @@ function UPneg10() {
 // =================================================
 function UPneg9() {
   if (game.numbers >= 0.0000000001) {
-    if (game.upNeg9Count >= game.upNeg9Max-1) {
-      game.upNeg9Count += 1
-      game.numbers += 0.0000000001
-      document.getElementById("number").innerHTML = formatNum(game.numbers);
-      document.getElementById("upneg9-buy-count").innerHTML = "Buy: " + game.upNeg9Count + "/5";
-      document.getElementById("upneg9-cost").innerHTML = "Upgrade Maxed";
-      document.getElementById("upgrade-neg9").disabled = true;
-      return;
-    };
-    game.upNeg9Count += 1;
-    game.numbers += 0.0000000001;
-    document.getElementById("number").innerHTML = formatNum(game.numbers);
-    document.getElementById("upneg9-buy-count").innerHTML = "Buy: " + game.upNeg9Count + "/5";
-    return;
-  }
-}
-// =================================================
-function UPneg9() {
-  if (game.numbers >= 0.0000000001) {
-    if (game.upNeg9Count >= game.upNeg9Max-1) {
+    if (game.upNeg9Count >= 5-1) {
       game.upNeg9Count += 1
       game.numbers += 0.0000000001
       document.getElementById("number").innerHTML = formatNum(game.numbers);
@@ -62,7 +43,7 @@ function UPneg9() {
 // =================================================
 function UPneg8() {
   if (game.numbers >= 0.0000000006) {
-    if (game.upNeg8Count >= game.upNeg8Max-1) {
+    if (game.upNeg8Count >= 2-1) {
       game.upNeg8Count += 1
       game.numbers += 0.0000000002
       document.getElementById("number").innerHTML = formatNum(game.numbers);
@@ -75,6 +56,37 @@ function UPneg8() {
     game.numbers += 0.0000000002;
     document.getElementById("number").innerHTML = formatNum(game.numbers);
     document.getElementById("upneg8-buy-count").innerHTML = "Buy: " + game.upNeg8Count + "/2";
+    return;
+  }
+}
+// =================================================
+function UPneg7() {
+  if (game.numbers >= 0.000000001) {
+    if (game.upNeg7Count >= 2-1) {
+      game.upNeg7Count += 1
+      game.numbers *= 3
+      document.getElementById("number").innerHTML = formatNum(game.numbers);
+      document.getElementById("upneg7-buy-count").innerHTML = "Buy: " + game.upNeg7Count + "/2";
+      document.getElementById("upneg7-cost").innerHTML = "Upgrade Maxed";
+      document.getElementById("upgrade-neg7").disabled = true;
+      return;
+    };
+    game.upNeg8Count += 1;
+    game.numbers *= 3;
+    document.getElementById("number").innerHTML = formatNum(game.numbers);
+    document.getElementById("upneg7-buy-count").innerHTML = "Buy: " + game.upNeg7Count + "/2";
+    return;
+  }
+}
+// =================================================
+function UPneg6() {
+  if (game.numbers >= 0.000000009) {
+    game.upNeg6Count += 1
+    game.numbers += 0.000000001
+    document.getElementById("number").innerHTML = formatNum(game.numbers);
+    document.getElementById("upneg6-buy-count").innerHTML = "Buy: " + game.upNeg6Count + "/1";
+    document.getElementById("upneg6-cost").innerHTML = "Upgrade Maxed";
+    document.getElementById("upgrade-neg6").disabled = true;
     return;
   }
 }
